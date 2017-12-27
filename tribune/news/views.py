84491f40682b96cd7176e2dbda08b66a6b9a3458
Collins njau <collinsnjau39@gmail.com>
2017-12-27 22:41:35 +0300

    I have created the view to display the current date and connected the view with a url

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+import datetime as dt
 
 #the welcome view
 def welcome(request):
     return HttpResponse("COLLINS NJAU")
+
+def newsOfTheDay(request):
+    date=dt.date.today()#get current date
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)        
