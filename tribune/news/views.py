32f835ef4fbf3032c99518bee39e06f109e92a05
Collins njau <collinsnjau39@gmail.com>
2017-12-27 22:19:38 +0300

    I have created my first view then connected the first view to a url then connected the url to the project

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+#the welcome view
+def welcome(request):
+    return HttpResponse("COLLINS NJAU")
