4bce97d753c3f9d59f8ea28720abe6512c151856
Collins njau <collinsnjau39@gmail.com>
2017-12-28 19:36:40 +0300

    I have created the articles model and created the relationships with other models

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -19,3 +19,10 @@ class tags(models.Model):
     #make model readable from the shell
     def __str__(self):
         return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)#many yo many relationship
+    pub_date=models.DateTimeField(auto_now_add=True)#get date when article was published
