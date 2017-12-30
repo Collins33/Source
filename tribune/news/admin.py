39309c25b9f5b1b7b446874f7a69a1022f13d167
Collins njau <collinsnjau39@gmail.com>
2017-12-30 16:31:03 +0300

    I have added the admin page and added the models to it

--- tribune/news/admin.py
+++ tribune/news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Editor,tags,Article
+
+admin.site.register(Editor)
+admin.site.register(tags)
+admin.site.register(Article)
