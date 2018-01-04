6be207a9feda7a23d3921690303404bffabc4480
Collins njau <collinsnjau39@gmail.com>
2018-01-04 15:02:46 +0300

    I have been able to add the path containing the images to be used

--- tribune/tribune/settings.py
+++ tribune/tribune/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -102,7 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
