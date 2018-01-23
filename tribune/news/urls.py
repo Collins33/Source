e962915533eae86124f470ee5626035ddb1ce794
Collins njau <collinsnjau39@gmail.com>
2018-01-23 11:29:00 +0300

    I have added the template for the home page

--- tribune/news/urls.py
+++ tribune/news/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name="welcome")
+    url(r'^$',views.welcome,name="welcome"),
     url(r'^today/',views.newsOfTheDay,name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.pastDaysNews,name='pastNews'),
     url(r'^search/',views.search_results,name="search_results"),
